6fb5d4c4aa88dfe2763ecd05a51c8ea203f238f1
Tony Lian <1040424979@qq.com>
2018-09-24 18:02:15 -0700

    Added remove

--- src/NamedTupleUtilities.jl
+++ src/NamedTupleUtilities.jl
@@ -1,3 +1,10 @@
 module NamedTupleUtilities
+export remove
+@generated function remove(a::NamedTuple{an}, ::Val{bn}) where {an, bn}
+    names = (filter(item -> item != bn, [n for n in an])...,)
+    types = Tuple{Any[ fieldtype(a, n) for n in names ]...}
+    vals = Any[ :(getfield(a, $(QuoteNode(n)))) for n in names ]
+    :( NamedTuple{$names,$types}(($(vals...),)) )
+end
 
 end # module
