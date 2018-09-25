eb2279a5559196740e95b8c5520ef854251640dd
Tony Lian <1040424979@qq.com>
2018-09-24 18:07:54 -0700

    Add documentation for the remove function

--- src/NamedTupleUtilities.jl
+++ src/NamedTupleUtilities.jl
@@ -1,5 +1,15 @@
 module NamedTupleUtilities
 export remove
+"""
+    remove(a::NamedTuple, v::Val{n})
+
+Remove a field `n` from the `a` if it is in `a`.
+
+```jldoctest
+julia>remove((a=1,b=2,c=3),Val(:c))
+(a = 1, b = 2)
+```
+"""
 @generated function remove(a::NamedTuple{an}, ::Val{bn}) where {an, bn}
     names = (filter(item -> item != bn, [n for n in an])...,)
     types = Tuple{Any[ fieldtype(a, n) for n in names ]...}
