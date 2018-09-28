dd512f66ca1e2916bc863d3251850ee56fad6fb1
Tony Lian <1040424979@qq.com>
2018-09-28 13:40:42 -0700

    add unit tests for remove

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,8 @@ using Test
 
 @testset "NamedTupleUtilities" begin
 
-@test 2 == 3
+@test remove((a = 1, b = 2, c = 3),Val(:c)) == (a = 1, b = 2)
+@test remove((a = 1, b = 2),Val(:c)) == (a = 1, b = 2)
+@inferred remove((a = 1, b = 2),Val(:c))
 
 end
