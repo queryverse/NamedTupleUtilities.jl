a8d18e9aeb59d350bf4594c2a61360d3de7b47cb
Tony Lian <1040424979@qq.com>
2018-09-29 21:16:07 -0700

    Add test for range function

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,4 +7,9 @@ using Test
 @test remove((a = 1, b = 2),Val(:c)) == (a = 1, b = 2)
 @inferred remove((a = 1, b = 2),Val(:c))
 
+@test range((a = 1, b = 2, c = 3),Val(:a),Val(:b)) == (a = 1, b = 2)
+@test range((a = 1, b = 2, c = 3),Val(:b),Val(:d)) == (b = 2, c = 3)
+@test range((a = 1, b = 2, c = 3),Val(:d),Val(:c)) == NamedTuple()
+@inferred range((a = 1, b = 2, c = 3),Val(:a),Val(:b))
+
 end
